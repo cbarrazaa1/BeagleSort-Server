d0902603d2fe58162027c8737d1fa8cab1fb2681
Cesar Barraza <cbarrazaa1@gmail.com>
2019-11-20 09:40:10 -0600

    fixed comparator function

--- src/algorithms/Compare.js
+++ src/algorithms/Compare.js
@@ -1,4 +1,4 @@
-module.exports = function compare(ascending, actual, comparedTo) {
+module.exports = function compare(actual, comparedTo, ascending) {
   if (ascending) {
     if (actual > comparedTo) {
       return true;
